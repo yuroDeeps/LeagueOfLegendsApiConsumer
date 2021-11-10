7bc06d0303ab8d357b09021c7a2f24983b5f1bd2
Yuro <nikodemnovtzki@gmail.com>
2021-11-10 22:35:22 +0100

    create lol api project

---
+++ src/main/java/pl/yuro/lolapi/LeagueOfLegendsApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package pl.yuro.lolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfLegendsApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOfLegendsApiConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/yuro/lolapi/LeagueOfLegendsApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.yuro.lolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOfLegendsApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
